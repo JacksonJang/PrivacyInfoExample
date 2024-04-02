1993beaa654431bea57c0d291ac34bd7454b3961
장효원 <hyo961015@naver.com>
2024-04-02 19:28:31 +0900

    PrivacyInfo 예제 추가

---
+++ PrivacyInfoExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PrivacyInfoExample
+//
+//  Created by 장효원 on 4/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
